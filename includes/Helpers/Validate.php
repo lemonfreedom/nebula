1cc4a6c007574ee2efecd512e27754caee458e33
Noah Zhang <226582@qq.com>
2022-08-11 09:13:44 +0800

    版权信息提交

--- includes/Helpers/Validate.php
+++ includes/Helpers/Validate.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of Nebula.
  *
- * (c) 2022 NoahZhang <nbacms@gmail.com>
+ * (c) 2022 Noah Zhang <nbacms@gmail.com>
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
--- install.php
+++ install.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Nebula.
+ *
+ * (c) 2022 Noah Zhang <nbacms@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 use Nebula\Request;
 use Nebula\Response;
 use Nebula\Helpers\MySQL;
